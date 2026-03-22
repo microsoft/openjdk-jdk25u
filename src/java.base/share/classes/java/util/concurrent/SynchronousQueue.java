06d0addffa36dbe81c6e3bb0124a79731530805c
Saint Wesonga <saint.wesonga@microsoft.com>
2026-03-21 23:44:33 -0600

    Run 09

--- src/java.base/share/classes/java/util/concurrent/SynchronousQueue.java
+++ src/java.base/share/classes/java/util/concurrent/SynchronousQueue.java
@@ -181,7 +181,7 @@ public class SynchronousQueue<E> extends AbstractQueue<E>
                         // writes waiter then reads item. On ARM64, CAS
                         // (ldaxr/stlxr) + plain load to a different field does
                         // NOT provide StoreLoad ordering.
-                        // GHA Run 08
+                        // GHA Run 09
                         VarHandle.fullFence();
                         Thread w = p.waiter;
                         cmpExHead(p, p.next);
