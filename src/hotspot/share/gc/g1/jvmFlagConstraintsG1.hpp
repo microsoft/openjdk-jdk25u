4e8a758a76755d6e7931acb0d1a4f01bc401c566
Ashay Rane <253344819+raneashay@users.noreply.github.com>
2026-06-05 13:23:43 -0500

    Constrain G1 interval to be a multiple of `interval_gran`
    Without this patch, TestOptionsWithRanges.java fails with a JVM crash.

--- src/hotspot/share/gc/g1/jvmFlagConstraintsG1.hpp
+++ src/hotspot/share/gc/g1/jvmFlagConstraintsG1.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -39,6 +39,7 @@
   f(size_t, G1HeapRegionSizeConstraintFunc)           \
   f(uint,  G1NewSizePercentConstraintFunc)           \
   f(uint,  G1MaxNewSizePercentConstraintFunc)        \
+  f(uintx, G1TimeBasedEvaluationIntervalMillisConstraintFunc) \
                                                       \
   /* G1 Subconstraints */                             \
   f(uintx,  MaxGCPauseMillisConstraintFuncG1)         \
